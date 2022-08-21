9232996f59457a9ec91f3eba54dfd9365c3497cf
Olexiy Buyanskyy <olexiyb@gmail.com>
2022-08-21 16:47:31 +0300

    export configs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './decorators/task.decorators';
+export * from './interfaces/module-config.interfaces';
+export * from './interfaces/worker.interfaces';
 export * from './decorators/worker.decorators';
 export * from './graphile-worker.module';
 export * from './services/worker.service';
